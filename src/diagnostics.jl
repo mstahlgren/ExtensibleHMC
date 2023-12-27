7ecb810e39fcd9fd5d373186c1a684a1e6946186
Martin Ståhlgren <martin.stahlgren@gmail.com>
2023-12-27 18:36:08 +0200

    Better plots and added extract method

--- src/diagnostics.jl
+++ src/diagnostics.jl
@@ -4,6 +4,8 @@ import Plots: Plots, plot, scatter, quiver!
 export effective_size, effective_size2, autocor, acceptance_rate
 export plot, scatter, density
 
+extract(samples::Vector{Sample{T}}, idx...) where T = [s.state[idx...] for s in samples]
+
 StatsBase.autocor(samples::Vector{Sample}) = autocor(Matrix(samples))
 
 #effective_size(x) = length(x) / (1 + 2 * sum(autocor(x)[2:end,:])/length(x[1].q))
@@ -17,12 +19,12 @@ function Base.summary(samples::Vector{Sample{T}}) where T
     println("Number of samples: ", length(samples))
     #println("Effective size: ", effective_size(samples))
     #println("Effective size 2: ", effective_size2(samples))
-    println("Acceptance rate: ", acceptance_rate(samples))
-    println("Trajectory length: ", quantile([length(s.path) for s in samples], (0, 0.1, 0.5, 0.9, 1)))
-    println("Posterior ll: ", quantile([s.ll for s in samples], ((0, 0.1, 0.5, 0.9, 1))))
+    println("Acceptance rate: ", round(acceptance_rate(samples); digits = 2))
+    println("Trajectory length: ", round.(quantile([length(s.path) for s in samples], (0, 0.1, 0.5, 0.9, 1)); digits = 3))
+    println("Posterior ll: ", round.(quantile([s.ll for s in samples], ((0, 0.1, 0.5, 0.9, 1))); digits = 3))
 end
 
-function Plots.plot(path::Vector{State}, id1 = 1, id2 = 2)
+#= function Plots.plot(path::Vector{State}, id1 = 1, id2 = 2)
     Q = reduce(hcat, [s.q for s in path])'
     P = reduce(hcat, [s.p for s in path])'
 
@@ -32,18 +34,18 @@ function Plots.plot(path::Vector{State}, id1 = 1, id2 = 2)
     scatter(Q[:,id1], Q[:,id2], legend = false)
     quiver!(Q[:,id1], Q[:,id2], quiver = (P[:,id1], P[:,id2]))
     quiver!(Q[:,id1], Q[:,id2], quiver = (QD[:,id1], QD[:,id2]))
+end =#
+
+function Plots.plot(samples::Vector{Sample{T}}, idx...) where T
+    extract(samples, idx) |> plot
 end
 
-function Plots.plot(samples::Vector{Sample}, idx = 1)
-    [s.state[idx...] for s in samples] |> plot
+function Plots.scatter(samples::Vector{Sample{T}}, idx₁, idx₂) where T
+    q₁, q₂ = extract(samples, idx₁...), extract(samples, idx₂...)
+    scatter(q₁, q₂)
 end
 
-function Plots.scatter(samples::Vector{Sample}, id1 = 1, id2 = 2)
-    M = Matrix(samples)
-    scatter(M[:,id1], M[:,id2])
-end
-
-function density(samples::Vector{Sample}, idx = 1)
+function density(samples::Vector{Sample}, idx...)
     S = [s.state[idx...] for s in samples]
     low = minimum(S)
     high = maximum(S)
