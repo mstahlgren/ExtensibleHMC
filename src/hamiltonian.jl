849fa50ebf24bf9fc08a9bfeb8fb43cac3e979ce
Martin Ståhlgren <martin.stahlgren@gmail.com>
2024-04-30 12:38:19 +0200

    Reverted to static Zygote backend

--- src/hamiltonian.jl
+++ src/hamiltonian.jl
@@ -1,14 +1,14 @@
 import LinearAlgebra: ⋅
+import Zygote: gradient
 
 export Hamiltonian, ∇, mass, gradient
 
 struct Hamiltonian{T <: Mass}
     U::Function # Unnormalized log posterior
     mass::T # LLᵀ decomposition
-    ad::Module # AD backend
 end
 
-gradient(H::Hamiltonian, s::AbstractArray) = H.ad.gradient(H.U, s)[1]
+gradient(H::Hamiltonian, s::AbstractArray) = gradient(H.U, s)[1]
 
 energy(H::Hamiltonian, s) = kinetic(H, s) + potential(H, s)
 
