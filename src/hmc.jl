2191fd6dd705172aa76b25c7ec44aa24696feb29
Martin Ståhlgren <martin.stahlgren@gmail.com>
2024-06-02 21:32:43 +0200

    Added divergence to HMC

--- src/hmc.jl
+++ src/hmc.jl
@@ -8,8 +8,10 @@ end
 function StatsBase.sample(ϕ::HMC, θ::Hamiltonian, q₀::AbstractVecOrMat)
     s₀ = s₁ = State(q₀, mass(θ) * randn(q₀ |> size), gradient(θ, q₀))
     for _ in 1:ϕ.L s₁ = leapfrog(θ, s₁, stepsize(ϕ)) end
-    δE = energy(θ, s₁) - energy(θ, s₀)
-    accepted = δE < 0.0 || exp(-δE) > rand()
+    E₁ = energy(θ, s₁)
+    δE = E₁ - energy(θ, s₀)
+    d = E₁ < - 1000
+    accepted = !d && (δE < 0.0 || exp(-δE) > rand())
     s = accepted ? s₁ : s₀
-    return Sample(q(s), potential(θ, s), accepted)
+    return Sample(q(s), potential(θ, s), accepted, d)
 end
