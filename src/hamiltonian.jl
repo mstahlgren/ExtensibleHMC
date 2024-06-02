46c541adb934e9ddf014aed4d39eb3be197684a5
Martin Ståhlgren <martin.stahlgren@gmail.com>
2024-06-02 21:17:35 +0200

    Name changes to Hamiltonian

--- src/hamiltonian.jl
+++ src/hamiltonian.jl
@@ -4,16 +4,16 @@ import Zygote: gradient
 export Hamiltonian, ∇, mass, gradient
 
 struct Hamiltonian{P, M <: Mass}
-    U::P # Unnormalized log posterior
+    density::P # Unnormalized log posterior
     mass::M # LLᵀ decomposition
 end
 
-gradient(H::Hamiltonian, s::AbstractArray) = gradient(H.U, s)[1]
+gradient(H::Hamiltonian, s::AbstractArray) = gradient(H.density, s)[1]
 
 energy(H::Hamiltonian, s) = kinetic(H, s) + potential(H, s)
 
 kinetic(H::Hamiltonian, s) = 0.5 * p(s)⋅(H.mass\p(s))
 
-potential(H::Hamiltonian, s) = -H.U(q(s))
+potential(H::Hamiltonian, s) = -H.density(q(s))
 
 mass(H::Hamiltonian) = H.mass
