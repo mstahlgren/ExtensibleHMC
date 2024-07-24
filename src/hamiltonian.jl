35fddce2d51983e0a1dff288add9d01484272cf4
Martin Ståhlgren <martin.stahlgren@gmail.com>
2024-07-24 18:52:40 +0300

    Moved ll inside state

--- src/hamiltonian.jl
+++ src/hamiltonian.jl
@@ -8,12 +8,12 @@ struct Hamiltonian{P, M <: Mass}
     mass::M # LLᵀ decomposition
 end
 
-gradient(H::Hamiltonian, s::AbstractArray) = withgradient(H.density, s)
+(H::Hamiltonian2)(s) = withgradient(H.density, s)
 
 energy(H::Hamiltonian, s) = kinetic(H, s) + potential(H, s)
 
 kinetic(H::Hamiltonian, s) = 0.5 * p(s)⋅(H.mass\p(s))
 
-potential(H::Hamiltonian, s) = -H.density(q(s))
+potential(::Hamiltonian, s) = -ll(s)
 
 mass(H::Hamiltonian) = H.mass
