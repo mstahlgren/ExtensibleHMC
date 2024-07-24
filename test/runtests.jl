610d25d968ac4e5618788f7838e35895cbfb5f7d
Martin Ståhlgren <martin.stahlgren@gmail.com>
2024-07-25 00:18:46 +0300

    Cleaned up tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,18 @@
 using ExtensibleHMC
 using Test
 
-H = Hamiltonian(x->x'x, UnitMass())
+n = 2
+
+hu = Hamiltonian(x->-x'x, UnitMass())
+hd = Hamiltonian(x->-x'x, DiagMass(ones(n)))
+
+s_hmc = sample(HMC(0.05, 10), H, zeros(n), n)
+
+s_nuts_unit = sample(NUTS(0.05), hu, zeros(n), n)
+s_nuts_diag = sample(NUTS(0.05), hd, zeros(n), n)
 
 @testset "Samplers" begin
-    S_HMC = sample(HMC(0.05, 10), H, zeros(3), 3)
-    @test S_HMC |> length == 3
-
-    S_NUTS_UNIT = sample(NUTS(0.05), Hamiltonian(x->x'x, UnitMass()), zeros(3), 3)
-    S_NUTS_DIAG = sample(NUTS(0.05), Hamiltonian(x->x'x, DiagMass(ones(3))), zeros(3), 3)
-    #S_NUTS_DENS = sample(NUTS(0.05), Hamiltonian(x->x'x, DenseMass()), zeros(3), 3)
-    @test S_NUTS_UNIT |> length == 3
-    @test S_NUTS_DIAG |> length == 3
+    @test S_HMC |> length == n
+    @test s_nuts_unit |> length == n
+    @test s_nuts_diag |> length == n
 end
