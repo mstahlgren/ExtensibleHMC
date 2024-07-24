029d5659fe7bd3be5b1e13ca1ffd15b381e60698
Martin Ståhlgren <martin.stahlgren@gmail.com>
2024-07-24 19:55:18 +0300

    Removed ll as separate return from leapfrog

--- src/nuts.jl
+++ src/nuts.jl
@@ -16,19 +16,19 @@ end
 function StatsBase.sample(ϕ::NUTS, θ, q₀, verbose = false)
     s₀ = State(θ, q₀)
     u = log(rand()) - energy(θ, s₀)
-    s⁻, s⁺, s₁, j, n, t, d, ll = s₀, s₀, s₀, 0, 1, false, false, θ.density(q₀)
+    s⁻, s⁺, s₁, j, n, t, d = s₀, s₀, s₀, 0, 1, false, false
     while j <= 8
         if verbose print("New branch :: j ", j) end
-        if rand(Bool) s⁻, _, s′, n′, t, d, ll´ = buildleft(ϕ, θ, s⁻, u, j)
-        else _, s⁺, s′, n′, t, d, ll´ = buildright(ϕ, θ, s⁺, u, j) end
+        if rand(Bool) s⁻, _, s′, n′, t, d = buildleft(ϕ, θ, s⁻, u, j)
+        else _, s⁺, s′, n′, t, d = buildright(ϕ, θ, s⁺, u, j) end
         if verbose println(" :: internal uturn ", t, " :: diverged ", d, " :: uturn ", uturn(s⁻, s⁺)) end
         if t || d break end
-        if n′/n > rand() s₁ = s′; ll = ll´ end
+        if n′/n > rand() s₁ = s′ end
         if uturn(s⁻, s⁺) break end
         n += n′
         j += 1
     end
-    return Sample(q(s₁), ll, q₀ != q(s₁), d, j == 8)
+    return Sample(q(s₁), s₁.ll, q₀ != q(s₁), d, j == 8)
 end
 
 function buildleft(ϕ, θ, s, u, j)
@@ -37,22 +37,22 @@ function buildleft(ϕ, θ, s, u, j)
     if d₁ return s⁻, s⁺, s₁, n₁, t₁, true, Inf end
     s⁻, _, s₂, n₂, t₂, d₂, ll₂ = buildleft(ϕ, θ, s⁻, u, j-1)
     n = n₂ / (n₁ + n₂) > rand()
-    return s⁻, s⁺, n ? s₁ : s₂, n₁ + n₂, t₁ || t₂ || uturn(s⁻, s⁺), d₂, n ? ll₁ : ll₂
+    return s⁻, s⁺, n ? s₁ : s₂, n₁ + n₂, t₁ || t₂ || uturn(s⁻, s⁺), d₂
 end
 
 function buildright(ϕ, θ, s, u, j)
     if iszero(j) return buildleaf(θ, s, u, stepsize(ϕ)) end
-    s⁻, s⁺, s₁, n₁, t₁, d₁, ll₁ = buildright(ϕ, θ, s, u, j-1)
+    s⁻, s⁺, s₁, n₁, t₁, d₁ = buildright(ϕ, θ, s, u, j-1)
     if d₁ return s⁻, s⁺, s₁, n₁, t₁, true, Inf end
-    _, s⁺, s₂, n₂, t₂, d₂, ll₂ = buildright(ϕ, θ, s⁻, u, j-1)
+    _, s⁺, s₂, n₂, t₂, d₂ = buildright(ϕ, θ, s⁻, u, j-1)
     n = n₂ / (n₁ + n₂) > rand()
-    return s⁻, s⁺, n ? s₁ : s₂, n₁ + n₂, t₁ || t₂ || uturn(s⁻, s⁺), d₂, n ? ll₁ : ll₂
+    return s⁻, s⁺, n ? s₁ : s₂, n₁ + n₂, t₁ || t₂ || uturn(s⁻, s⁺), d₂
 end
 
 function buildleaf(θ, s, u, ϵ)
-    ll, s′ = leapfrog(θ, s, ϵ)
-    E = kinetic(θ, s′) - ll
+    s′ = leapfrog(θ, s, ϵ)
+    E = energy(θ, s′)
     n = Int(-E >= u)
     d = -E < u - 1000
-    return s′, s′, s′, n, false, d, ll
+    return s′, s′, s′, n, false, d
 end
