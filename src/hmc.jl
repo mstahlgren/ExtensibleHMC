45498b8df4f744c495346952e02209c6b2f29512
Martin Ståhlgren <martin.stahlgren@gmail.com>
2024-08-23 16:12:37 +0200

    Added max E diff and cleaned up HMC

--- src/hmc.jl
+++ src/hmc.jl
@@ -5,16 +5,18 @@ export HMC
 struct HMC <: Sampler
     ϵ::Float64
     L::Int
+    max_ΔE::Float64
 end
 
+HMC(ϵ, L) = HMC(ϵ, L, 1000)
+
 function StatsBase.sample(ϕ::HMC, θ, q₀; verbose = false)
-    s₀ = State(θ, q₀)
-    s₁ = leapfrog(θ, s₀, stepsize(ϕ))
-    for _ in 2:ϕ.L s₁ = leapfrog(θ, s₁, stepsize(ϕ)) end
-    E₁ = energy(θ, s₁)
-    δE = E₁ - energy(θ, s₀)
-    d = -abs(δE) < - 1000
-    accepted = !d && (δE < 0.0 || exp(-δE) > rand())
+    s₀ = s₁ = State(θ, q₀)
+    for _ in 1:ϕ.L s₁ = leapfrog(θ, s₁, ϕ.ϵ) end
+    ΔE = energy(s₁) - energy(s₀)
+    d = abs(ΔE) > ϕ.max_ΔE
+    p = min(1.0, exp(-ΔE))
+    accepted = !d && (ΔE < 0.0 || rand() < p)
     s = accepted ? s₁ : s₀
-    return Sample(q(s), potential(θ, s), ϕ.L, accepted, d)
+    return Sample(q(s), ll(s), ϕ.L, p, d, false)
 end
