17897a358cea1957a03065f0aac2f40350ed9d91
Martin Ståhlgren <martin.stahlgren@gmail.com>
2024-08-23 16:13:53 +0200

    Added adaptation and test functions

--- src/ExtensibleHMC.jl
+++ src/ExtensibleHMC.jl
@@ -1,5 +1,11 @@
 module ExtensibleHMC
 
+export rosenbrock
+
+rosenbrock(x, y, a, b) = (a - x)^2 + b*(y - x^2)^2
+
+rosenbrock(x) = rosenbrock(x[1], x[2], 1, 100)
+
 include("state.jl")
 include("mass.jl")
 include("hamiltonian.jl")
@@ -7,6 +13,7 @@ include("sample.jl")
 include("hmc.jl")
 include("nuts.jl")
 include("mnuts.jl")
+include("adapt.jl")
 include("diagnostics.jl")
 
 end # module ExtensibleHMC
