f984e7644a440fc41c7274abd26b6d412ecb5403
Martin Ståhlgren <martin.stahlgren@gmail.com>
2024-08-23 16:20:15 +0200

    Added testcase for MNUTS

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,6 +11,8 @@ s_hmc = sample(HMC(0.05, 10), hu, zeros(n), n)
 s_nuts_unit = sample(NUTS(0.05), hu, zeros(n), n)
 s_nuts_diag = sample(NUTS(0.05), hd, zeros(n), n)
 
+s_nuts_unit = sample(MNUTS(0.05), hu, zeros(n), n)
+
 @testset "Samplers" begin
     @test s_hmc |> length == n
     @test s_nuts_unit |> length == n
