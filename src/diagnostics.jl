30f07ccf8e7df84777a878802f056797628680ea
Martin Ståhlgren <martin.stahlgren@gmail.com>
2024-08-24 23:16:26 +0200

    Cleaned up diagnostics

--- src/diagnostics.jl
+++ src/diagnostics.jl
@@ -1,50 +1,45 @@
 import StatsBase: mean, quantile, Histogram, fit, autocor
 import Plots: Plots, plot, scatter
 
-export summary, acceptrate
+export summary, acceptrate, effectivesize
 export plot, scatter, density
 
 const Samples{T} = Vector{Sample{T}}
 
-extract(samples::Vector{Sample{T}}, idx...) where T = [s.value[idx...] for s in samples]
+StatsBase.autocor(x::Samples) = autocor(reduce(hcat, [s.value for s in x])')
 
-#StatsBase.autocor(samples::Vector{Sample}) = autocor(Matrix(samples))
+effectivesize(x::Samples) = mean(length(x) ./ (2 .* [sum(a) for a in eachcol(autocor(x))] .- 1))
 
-#effective_size(x) = length(x) / (1 + 2 * sum(autocor(x)[2:end,:])/length(x[1].q))
-
-#effective_size2(x) = length(x) / (1 + 2 * sum(abs.(autocor(x)[2:end,:]))/length(x[1].q))
-
-naccepted(x) = sum(s.accepted for s in x)
+acceptrate(x) = sum(s.acceptrate * s.nsteps for s in x) / sum(s.nsteps for s in x)
 
 ndivergences(x) = sum(s.diverged for s in x)
 
-function Base.summary(samples::Vector{Sample{T}}) where T
-    n = length(samples)
+function Base.summary(S::Vector{Sample{T}}) where T
     qs = (0, 0.1, 0.5, 0.9, 1)
     rnd = x -> round(x; digits = 3)
-    println("Number of free variables: ", length(samples[1].value))
-    println("Number of samples: ", n)
-    println("Autocoorelation τ¹⁻³: ", rnd.(autocor(extract(samples, 1))[2:4]))
-    println("Acceptance rate: ", rnd(naccepted(samples) / n))
-    println("Divergance rate: ", rnd(ndivergences(samples) / n))
-    println("Number of steps: ", rnd.(quantile([s.nsteps for s in samples], qs))),
-    println("Posterior ll: ", rnd.(quantile([s.ll for s in samples], qs)))
+    println("Number of free variables: ", length(S[1].value))
+    println("Number of samples: ", length(S))
+    println("Autocorrelation τ¹⁻³: ", rnd.([mean(s) for s in eachrow(autocor(S)[2:4])]))
+    println("Acceptance rate: ", rnd(acceptrate(S)))
+    println("Divergance rate: ", rnd(ndivergences(S) / length(S)))
+    println("Number of steps: ", rnd.(quantile([s.nsteps for s in S], qs)))
+    println("Posterior ll: ", rnd.(quantile([s.ll for s in S], qs)))
     return nothing
 end
 
 # QT plot
 function Plots.plot(samples::Vector{Sample{T}}, idx...) where T
-    extract(samples, idx...) |> plot
+    [s.value[idx...] for s in samples] |> plot
 end
 
 # QQ scatter plot
 function Plots.scatter(samples::Vector{Sample{T}}, id₁, id₂) where T
-    q₁, q₂ = extract(samples, id₁), extract(samples, id₂)
-    scatter(q₁, q₂, xlim = (-3,3), ylim = (-3,3), aspect_ratio = :equal)
+    q = reduce(hcat, [s.value[[id₁, id₂]] for s in samples])
+    scatter(view(q,:,1), view(q,:,2), xlim = (-3,3), ylim = (-3,3), aspect_ratio = :equal)
 end
 
-function density(samples::Vector{Sample{T}}, idx...) where T
-    S = [s.value[idx...] for s in samples]
+function density(samples::Vector{Sample{T}}, idx) where T
+    S = [s.value[idx] for s in samples]
     low = minimum(S)
     high = maximum(S)
     bins = low:(high-low)/32:high
