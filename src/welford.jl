b9c4cb2327533b7cf98d7ba49f0bdbd4053dd42a
Martin Ståhlgren <martin.stahlgren@gmail.com>
2024-10-18 12:24:12 -0400

    Added welford colsum

--- src/welford.jl
+++ src/welford.jl
@@ -1,6 +1,6 @@
 import Statistics: Statistics
 
-export Welford, update, var
+export Welford, update!, var, colsum
 
 struct Welford{T,S <: AbstractVecOrMat{T}}
     n::Int
@@ -14,15 +14,26 @@ Statistics.mean(w::Welford) = w.mean
 
 Statistics.var(w::Welford) = w.M2 / (w.n - 1)
 
-function update(w::Welford, values::Vector{T}) where T <: AbstractVecOrMat
-    for v in values w = update(w, v) end
+function update!(w::Welford, samples::Vector{Samples})
+    for s in samples w = update!(w, s.value) end
     return w
 end
 
-function update(w::Welford, value)
+function update!(w::Welford, value)
     n = w.n + 1
     delta = value - w.mean
     w.mean .= w.mean .+ delta ./ n
-    w.M2 .+= delta .* (value .- mean)
-    return w
+    w.M2 .+= delta .* (value .- w.mean)
+    return Welford(n, w.mean, w.M2)
+end
+
+function colsum(w::Welford)
+    nb, na, m, m2 = w.n, w.n, w.mean[:, 1], w.M2[:, 1]
+    for i in 2:size(w.mean)[2]
+        δ = view(w.mean, :, i) - m
+        m .+= δ .* nb ./ (na + nb)
+        m2 .+= view(w.M2, :, i) .+ δ.^2 .* na * nb / (na + nb)
+        na += nb
+    end
+    Welford{eltype(m), typeof(m)}(na, m, m2)
 end
