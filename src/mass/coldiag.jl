e488cfb9941e3daa0c738c05cb38f2e33c567c02
Martin Ståhlgren <martin.stahlgren@gmail.com>
2025-02-20 17:06:30 +0100

    Switched to callable Mass

--- src/mass/coldiag.jl
+++ src/mass/coldiag.jl
@@ -7,7 +7,7 @@ end
 
 ColDiag(R, C) = ColDiag(R, C, 1, ones(R, C))
 
-function ColDiag(S::Samples, M = ColDiag(size(S[1].value)...), ν = 0.0)
+function (m::ColDiag)(S::Samples, ν = 0.0)
     expanded = reduce(hcat, vec(s.value) for s in S)
     variance, frac = var(expanded, dims = 2, corrected = false), round(ν * M.N)
     ColDiag(M.R, M.C, Int(frac + length(S)), (frac / M.N) .* M⁻¹ .+ variance)
