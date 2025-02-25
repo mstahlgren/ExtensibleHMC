0718d3057150d404e79c662b90d409adf354fc4d
Martin Ståhlgren <martin.stahlgren@gmail.com>
2025-02-25 03:26:20 +0200

    Wierd fix for autocor

--- src/diagnostics.jl
+++ src/diagnostics.jl
@@ -1,8 +1,9 @@
 samples(x::Samples, f = identity) = [f(s.value) for s in x]
 
-StatsBase.autocor(x::Samples) = autocor(reduce(hcat, samples(x, vec))')
+#StatsBase.autocor(x::Samples) = autocor(reduce(hcat, samples(x, vec))')
+autocor2(x::Samples) = autocor(reduce(hcat, samples(x, vec))')
 
-ess(x::Samples, agg = maximum) = length(x) / (2 * agg(sum(autocor(x), dims = 1)) - 1)
+ess(x::Samples, agg = maximum) = length(x) / (2 * agg(sum(autocor2(x), dims = 1)) - 1)
 
 acceptrate(x::Samples) = sum(s.acceptrate * s.nsteps for s in x) / sum(s.nsteps for s in x)
 
@@ -16,12 +17,12 @@ function Base.summary(S::Samples)
     println("Number of samples: ", length(S))
     println("Minimum ESS: ", rnd(ess(S)))
     println("Average ESS: ", rnd(ess(S, mean)))
-    println("Highest autocorr: ", argmax(vec(sum(autocor(S), dims = 1))))
+    println("Highest autocorr: ", argmax(vec(sum(autocor2(S), dims = 1))))
     println("Acceptance rate: ", rnd(acceptrate(S)))
     println("Divergence rate: ", rnd(ndivergences(S) / length(S)))
     println("N minESS per 1000 steps: ", rnd(1000*ess(S) / sum(s.nsteps for s in S)))
     println("Number of steps: ", rnd.(quantile([s.nsteps for s in S], qs)))
-    println("Autocorrelation τ¹⁻³: ", rnd.([mean(s) for s in eachrow(autocor(S)[2:4])]))
+    println("Autocorrelation τ¹⁻³: ", rnd.([mean(s) for s in eachrow(autocor2(S)[2:4])]))
     println("Posterior ll: ", rnd.(quantile([s.ll for s in S], qs)))
     return nothing
 end
