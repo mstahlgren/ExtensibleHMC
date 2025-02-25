9b3a59bd9822330b44c5f75c42ab80830cb66006
Martin Ståhlgren <martin.stahlgren@gmail.com>
2025-02-26 00:41:50 +0200

    Cleaned up diagnostics

--- src/diagnostics.jl
+++ src/diagnostics.jl
@@ -1,9 +1,10 @@
-samples(x::Samples, f = identity) = [f(s.value) for s in x]
+Base.values(x::Samples) = reduce(hcat, s.value for s in x)
 
-#StatsBase.autocor(x::Samples) = autocor(reduce(hcat, samples(x, vec))')
-autocor2(x::Samples) = autocor(reduce(hcat, samples(x, vec))')
+StatsBase.autocor(x::Samples) = autocor(Base.values(x)')
 
-ess(x::Samples, agg = maximum) = length(x) / (2 * agg(sum(autocor2(x), dims = 1)) - 1)
+ess(x::Matrix, N, agg) = agg(vec(N ./ (2 .* sum(x, dims = 1) .- 1)))
+
+ess(x::Samples, agg) = ess(autocor(x), length(x), agg)
 
 acceptrate(x::Samples) = sum(s.acceptrate * s.nsteps for s in x) / sum(s.nsteps for s in x)
 
@@ -11,20 +12,20 @@ ndivergences(x::Samples) = sum(s.diverged for s in x)
 
 # Samples per nsteps
 function Base.summary(S::Samples)
+    rnd(x) = round(x; digits = 3)
     qs = (0, 0.1, 0.5, 0.9, 1)
-    rnd = x -> round(x; digits = 3)
+    N, ac = length(S), autocor(S)
     println("Number of free variables: ", length(S[1].value))
-    println("Number of samples: ", length(S))
-    println("Minimum ESS: ", rnd(ess(S)))
-    println("Average ESS: ", rnd(ess(S, mean)))
-    println("Highest autocorr: ", argmax(vec(sum(autocor2(S), dims = 1))))
+    println("Number of samples: ", N)
+    println("Minimum ESS: ", rnd(ess(ac, N, minimum)))
+    println("Average ESS: ", rnd(ess(ac, N, mean)))
+    println("Highest autocorr: ", ess(ac, N, argmax))
     println("Acceptance rate: ", rnd(acceptrate(S)))
-    println("Divergence rate: ", rnd(ndivergences(S) / length(S)))
-    println("N minESS per 1000 steps: ", rnd(1000*ess(S) / sum(s.nsteps for s in S)))
+    println("Divergences: ", rnd(ndivergences(S)))
+    println("N minESS per 100 steps: ", rnd(100*ess(S, minimum) / sum(s.nsteps for s in S)))
     println("Number of steps: ", rnd.(quantile([s.nsteps for s in S], qs)))
-    println("Autocorrelation τ¹⁻³: ", rnd.([mean(s) for s in eachrow(autocor2(S)[2:4])]))
-    println("Posterior ll: ", rnd.(quantile([s.ll for s in S], qs)))
-    return nothing
+    println("Autocorrelation τ¹⁻³: ", rnd.(vec(mean(ac[2:4, :], dims = 1))))
+    print("Posterior ll: ", rnd.(quantile([s.ll for s in S], qs)))
 end
 
 # QT plot
