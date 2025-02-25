106bdb1751410e6fae629658a4c2ead4a35f23b7
Martin Ståhlgren <martin.stahlgren@gmail.com>
2025-02-25 23:29:20 +0200

    New model interface

---
+++ src/model.jl
@@ -0,0 +1,17 @@
+abstract type AbstractModel end
+
+function sampler end
+
+function hamiltonian end
+
+function init end
+
+function sample(m::AbstractModel, n::Int) 
+    sample(sampler(m), hamiltonian(m), init(m), n)
+end
+
+function adapt(m::AbstractModel, epochs, n)
+    ϕ, ψ, q = adapt(sampler(m), hamiltonian(m), init(m), epochs, n)
+    set(m; mass = ψ) |> x->set(x; step = ϕ) |> x->set(m; init = q)
+end
+
