6f6666cd78e3ddf8f012075879fcc5c6a3d4adc3
Martin Ståhlgren <martin.stahlgren@gmail.com>
2025-02-26 01:58:28 +0200

    Added size as type parameter

--- src/mass/unit.jl
+++ src/mass/unit.jl
@@ -1,13 +1,13 @@
-struct UnitMass{N} <: AbstractMass end
+struct UnitMass{N} <: AbstractMass{N} end
 
 UnitMass(N::Int) = UnitMass{N}()
 
 (m::UnitMass)(samples, ν = 0.0) = m
 
-Base.rand(m::UnitMass{N}) where N = randn(N)
+Base.rand(m::UnitMass) = m |> length |> randn
 
 Base.:\(::UnitMass, x) = x
 
-Base.:+(m::UnitMass, y::UnitMass) = m
+Base.:+(m::UnitMass, ::UnitMass) = m
 
 LinearAlgebra.logabsdet(::UnitMass) = 0
