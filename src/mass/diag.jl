b06d97c4c4b6e145aa258da814e6df4ee051ac82
Martin Ståhlgren <martin.stahlgren@gmail.com>
2025-02-27 02:40:12 +0200

    Adapted to latest api

--- src/mass/diag.jl
+++ src/mass/diag.jl
@@ -1,21 +1,23 @@
-struct DiagMass{N} <: AbstractMass{N}
-    M⁻¹::Vector{Float64}
-    nₛ::Int
+struct DiagMass{D} <: AbstractMass{D}
+    M⁻¹::Array{Float64, D}
+    N::Int
 end
 
-DiagMass(N) = DiagMass{N}(ones(N), 1)
+DiagMass(size...) = DiagMass{length(size)}(ones(size...), 1)
 
-function (m::DiagMass)(S, ν = 0.0)
-    V = var(values(S), dims = 2, corrected = false)
-    n₁ = round(ν * m.N)
-    nₛ = Int(N₀′) + length(S)
-    typeof(m)((n₁ .* m.M⁻¹ .+ length(S) .* V) / nₛ, nₛ)
+(m::DiagMass)(S, ν) = begin
+    V = reshape(var(values(S), dims = 2, corrected = false), size(m))
+    n₋ = round(ν * m.N)
+    nₛ = Int(n₋) + length(S)
+    typeof(m)((n₋ .* m.M⁻¹ .+ length(S) .* V) / nₛ, nₛ)
 end
 
-Base.:+(x::T, y::T) where T <: DiagMass = T((x.M⁻¹ .+ y.M⁻¹) ./ 2, x.N + y.N)
+Base.size(m::DiagMass) = size(m.M⁻¹)
 
-Base.rand(m::DiagMass) = sqrt.(1 ./ m.M⁻¹) .* randn(length(m))
+Base.rand(m::DiagMass) = sqrt.(1 ./ m.M⁻¹) .* randn(size(m))
 
 Base.:\(m::DiagMass, x) = m.M⁻¹ .* x
+
+Base.:+(x::T, y::T) where T <: DiagMass = T((x.M⁻¹ .+ y.M⁻¹) ./ 2, x.N + y.N)
 
 LinearAlgebra.logabsdet(m::DiagMass) = -sum(log.(m.M⁻¹))
