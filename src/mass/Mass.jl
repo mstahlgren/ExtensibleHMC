549387729c01e08a9b8aed2cbd2148ca58fc3983
Martin Ståhlgren <martin.stahlgren@gmail.com>
2025-07-09 16:52:36 +0200

    Added tridiag

--- src/mass/Mass.jl
+++ src/mass/Mass.jl
@@ -4,6 +4,7 @@ Base.length(m::AbstractMass) = prod(size(m))
 
 include("unit.jl")
 include("diag.jl")
+include("tridiag.jl")
 #include("repdense.jl")
 #include("tridiag.jl")
 #include("diagplus.jl")
