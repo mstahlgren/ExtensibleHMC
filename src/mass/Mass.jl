12a13b2d69448b53e38113686f7c697f48566f75
Martin Ståhlgren <martin.stahlgren@com>
2025-08-30 16:59:31 +0200

    Added robust dense mass

--- src/mass/Mass.jl
+++ src/mass/Mass.jl
@@ -4,7 +4,5 @@ Base.length(m::AbstractMass) = prod(size(m))
 
 include("unit.jl")
 include("diag.jl")
+include("robust.jl")
 include("tridiag.jl")
-#include("repdense.jl")
-#include("tridiag.jl")
-#include("diagplus.jl")
